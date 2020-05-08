c673dd72f3a4e7464c70f9c30497e0cf2765ea3a
HJL <32661012+CS594398574@users.noreply.github.com>
2020-05-08 16:02:40 +0800

    Update train_ResNet.py

--- Face_Detection_SSD/train_ResNet.py
+++ Face_Detection_SSD/train_ResNet.py
@@ -6,8 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from keras.preprocessing.image import img_to_array
 from keras.utils import np_utils
-#from pyimagesearch.nn.conv.lenet import LeNet
-from pyimagesearch.nn.conv.resnet import ResNet
+from nn.conv.resnet import ResNet
 from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
